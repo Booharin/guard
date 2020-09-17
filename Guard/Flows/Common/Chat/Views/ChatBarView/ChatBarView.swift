5e0d1cc587c303a1f689a3fa186d0f18f61e7e94
Alexandr <AIBukharin@sberbank.ru>
2020-09-17 21:20:17 +0300

    Added ChatBarView

---
+++ Guard/Flows/Common/Chat/Views/ChatBarView/ChatBarView.swift
@@ -0,0 +1,19 @@
+//
+//  ChatBarView.swift
+//  Guard
+//
+//  Created by Alexandr Bukharin on 17.09.2020.
+//  Copyright © 2020 ds. All rights reserved.
+//
+
+import UIKit
+
+final class ChatBarView: UIView {
+	init() {
+		super.init(frame: .zero)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
